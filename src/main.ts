76a67880bea44a3f03f3c69e53f10d20fac7b839
Shpatel <daniilsapel@gmail.com>
2025-09-15 14:40:33 +0300

    fix auth

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,26 @@ async function bootstrap() {
   
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: [
-      'http://194.62.19.106:3000'
+  origin: [
+      'http://shop.montegroup.by',
+      'https://shop.montegroup.by',
+      'http://localhost:4200',
+      'http://194.62.19.106'
     ],
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+      'Origin',
+      'Access-Control-Request-Method',
+      'Access-Control-Request-Headers'
+    ],
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 
   await app.listen(process.env.PORT || 3000);
