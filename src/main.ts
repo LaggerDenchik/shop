d4d5164edfe108a4d37751dc5cbd997b399ff1c2
Shpatel <daniilsapel@gmail.com>
2025-09-24 15:43:24 +0300

    fix cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,30 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'http://shop.montegroup.by');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
-      res.status(200).end();
-    } else {
-      next();
-    }
-  });
+  // app.use((req, res, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Origin', 'http://shop.montegroup.by');
+  //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  //     res.status(200).end();
+  //   } else {
+  //     next();
+  //   }
+  // });
 
   app.enableCors({
-    origin: [
-      'http://shop.montegroup.by',
-      'https://shop.montegroup.by',
-      'http://localhost:4200',
-      'http://45.87.219.107'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+    origin: 
+    [
+      'http://localhost:4200', // локальный front
+      'http://shop.montegroup.by', // front
+      'https://shop.montegroup.by', // HTTPS версия
+      'http://45.87.219.107:8080', // backend
+    ]
+    ,methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    ,allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+    ,credentials: true
+    ,preflightContinue: false
+    ,optionsSuccessStatus: 204
   });
 
   app.useGlobalPipes(new ValidationPipe());
