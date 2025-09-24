56e7eb8f6fb42da2b8d384bea9142b896c5c4171
Shpatel <daniilsapel@gmail.com>
2025-09-24 13:02:30 +0300

    clean trash

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  
-  
-  app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: [
-      'http://194.62.19.106:3000'
-    ],
-    credentials: true
+  const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'http://shop.montegroup.by');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+      res.status(200).end();
+    } else {
+      next();
+    }
   });
 
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors({
+    origin: [
+      'http://shop.montegroup.by',
+      'https://shop.montegroup.by',
+      'http://localhost:4200',
+      'http://45.87.219.107'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  
+  await app.listen(3000);
 }
 bootstrap();
